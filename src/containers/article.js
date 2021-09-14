bb3c8bb81cf29acade898bc6e10749b1b480eba9
mj2360 <mj2360@nyu.edu>
2021-09-14 08:45:37 -0500

    added articles

---
+++ src/containers/article.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+function Article(){
+    const articleTitle = 'Single Article'
+    return(
+        <div>
+            <h1>{articleTitle}</h1>
+        </div>
+
+    );
+}
+
+export default Article; 
---
+++ src/containers/articleListing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ArticleListing(){
+    return(
+        <div>
+            <h1>{articleTitle}</h1>
+        </div>
+    );
+}
